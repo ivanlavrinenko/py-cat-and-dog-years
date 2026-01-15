5202cdfe53f7d18fe051c6dd5f91c106051c1644
Ivan Lavrinenko <lavrinenkoivan51@gmail.com>
2026-01-15 20:18:58 +0100

    edge cases added to test

--- app/test_main.py
+++ app/test_main.py
@@ -5,13 +5,16 @@ import pytest
 @pytest.mark.parametrize(
     "cat_years, dog_years, cat_and_dog_years_in_human_years",
     [
+        (0, 0, [0, 0]),
         (14, 14, [0, 0]),
         (15, 15, [1, 1]),
         (23, 23, [1, 1]),
         (24, 24, [2, 2]),
         (25, 25, [2, 2]),
+        (28, 28, [3, 2]),
         (28, 29, [3, 3]),
-        (31, 32, [3, 3])
+        (31, 32, [3, 3]),
+        (100, 100, [21, 17])
     ]
 )
 def test_get_cat_and_dog_years_are_correctly_converted(
@@ -21,3 +24,8 @@ def test_get_cat_and_dog_years_are_correctly_converted(
 ) -> None:
     assert get_human_age(cat_years,
                          dog_years) == cat_and_dog_years_in_human_years
+
+
+def test_function_raises_exception_when_no_arguments_provided() -> None:
+    with pytest.raises(TypeError):
+        get_human_age("cat_age", "dog_age")
