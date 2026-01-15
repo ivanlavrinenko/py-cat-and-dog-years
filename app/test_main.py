6d779a98bb81057bd89351ea3b2b478ab6e6c853
Ivan Lavrinenko <lavrinenkoivan51@gmail.com>
2026-01-15 20:03:00 +0100

    function and test are implemented

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,23 @@
 from app.main import get_human_age
+import pytest
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "cat_years, dog_years, cat_and_dog_years_in_human_years",
+    [
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (25, 25, [2, 2]),
+        (28, 29, [3, 3]),
+        (31, 32, [3, 3])
+    ]
+)
+def test_get_cat_and_dog_years_are_correctly_converted(
+        cat_years: int,
+        dog_years: int,
+        cat_and_dog_years_in_human_years: list[int]
+) -> None:
+    assert get_human_age(cat_years,
+                         dog_years) == cat_and_dog_years_in_human_years
